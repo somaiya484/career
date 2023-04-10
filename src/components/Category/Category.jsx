690b6bdefe28ebc409889e23176c16f75a97086b
somaiya shahin <somaiyashahin484@gmail.com>
2023-04-10 23:58:45 +0600

    category section style added

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import './Category.css'
 
 const Category = ({ category }) => {
-    const { name, amountOfJob } = category;
+    const { name, amountOfJob, img } = category;
     return (
         <div>
 
             {/* <div className=""> */}
                 <div className="col">
-                    <div className="card h-100">
-                        <img src="..." className="card-img-top" alt="..." />
+                    <div className="card h-100 singleCard">
+                        <img src={img} className="card-img-top w-25 ms-2" alt="..." />
                         <div className="card-body">
                             <h5 className="text-start card-title">{name}</h5>
-                            <p className="text-start card-text">{amountOfJob}</p>
+                            <p className="text-start card-text text-secondary">{amountOfJob}</p>
                         </div>
                     </div>
                 </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,19 @@ const Header = () => {
         <div className='header'>
             <nav className="navbar navbar-expand-lg bg-body-tertiary container">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand fs-2 fw-bold " href="#">Oplor</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className='navbar-nav me-auto mb-2 mb-lg-0'>
-                            <Link to="/statistics">Statistics</Link>
+                        <div className='navbar-nav me-auto mb-2 mb-lg-0 '>
+                            <Link to="/statistics" >Statistics</Link>
                             <Link to="/applied">Applied Jobs</Link>
                             <Link to="/blog">Blog</Link>
                         </div>
 
 
-                        <button className="btn " type="submit">Search</button>
+                        <button className="btn text-white py-2" type="submit">Star Applying</button>
                     </div>
                 </div>
             </nav>
