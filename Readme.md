7c5c4eef1f97867f103150e0d75bda077263d658
somaiya shahin <somaiyashahin484@gmail.com>
2023-04-12 22:28:16 +0600

    readme file added

---
+++ Readme.md
@@ -0,0 +1,20 @@
+## Header 
+A navbar is there
+
+## Home
+First full page is there. In Home component there is Header, Category, Features components are available
+
+## Category
+Some category and information are in there
+
+## Feature 
+All kinds of jobs features and also single job features are in this component.
+
+## Statistics
+assignment 1 to 7, all marks displayed in this component with chart
+
+## Blog
+Some Q&A are there
+
+## json file
+Some data stored in json file 
--- src/components/DisplayJobs/Displayjob.jsx
+++ src/components/DisplayJobs/Displayjob.jsx
@@ -6,27 +6,19 @@ const Displayjob = ({ job }) => {
     const { img, sector, companyName, location, salary, jobCategory } = job;
     return (
         <div className='container job-container'>
-            <div className='cart d-flex my-4'>
-                <div className='img-div'>
-                    <img src={img} />
-                </div>
-                <div className='review-details'>
-                    <h4 className='job-title'>{sector}</h4>
-                    <h5 className='light-dark'>{companyName}</h5>
-                    <div className='d-flex gap-2'>
-                        <div>
-                            <h6 className='job-T-T'>{jobCategory}</h6>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="..."/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                         </div>
-                        <button className='featuresBtn ms-3'>Full Time</button>
-                    </div>
-                    <div className='d-flex gap-4'>
-                        <h5 className='light-dark'>{location}</h5>
-                        <h5 className='light-dark'>Salary: {salary}</h5>
                     </div>
                 </div>
-                <Link to={`/jobs/${id}`}>
-                    <button className='d-block mt-3 btn text-white fw-semibold'>View Details </button>
-                </Link>
             </div>
         </div>
     );
